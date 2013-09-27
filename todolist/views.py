2afc01aa7871e312dce0ab52759329ed72ee270e
TinyX <hb1412@gmail.com>
2013-09-27 21:08:19 +0000

    save

--- todolist/views.py
+++ todolist/views.py
@@ -1,1 +1,25 @@
 from todolist.models import Event, EventClass
+
+def get_event_classes(request, user_id):
+    return True
+
+def get_events(request, user_id, class_id):
+    return True
+
+def add_event_class(request, user_id):
+    return True
+
+def add_event(request, user_id, class_id):
+    return True
+
+def remove_event_class(request, class_id):
+    return True
+
+def remove_event(request, event_id):
+    return True
+
+def update_event_classes_order(request, user_id):
+    return True
+
+def update_events_order(request, user_id, class_id):
+    return True
