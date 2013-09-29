29b9bd3dc6026991c65c3a769df7925ebed033c2
TinyX <hb1412@gmail.com>
2013-09-28 23:58:08 -0400

    update

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,14 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 from todolist import views
 
 urlpatterns = patterns('',
-    url(r'^cover/$', views.TodoIndexView.as_view(template_name='todo_cover.html',
-                                         title='2do-List'), name='todo_cover'),
-    url(r'^login/$', views.TodoIndexView.as_view(template_name='todo_cover.html',
-                                         title='2do-List | Login'), name='todo_login'),
-    url(r'^reg/$', views.TodoIndexView.as_view(template_name='todo_cover.html',
-                                         title='2do-List | Register'), name='todo_reg'),
-    url(r'^main/$', views.TodoIndexView.as_view(template_name='todo_cover.html',
-                                         title='2do-List'), name='todo_main'),
-
+    url(r'^cover/$', \
+        TemplateView.as_view(template_name='todo_cover.html'), name='todo_cover'),
+    url(r'^login/$', views.todo_login, name='todo_login'),
+    url(r'^reg/$', views.todo_reg, name='todo_reg'),
+    url(r'^main/$', \
+        TemplateView.as_view(template_name='todo_main.html'), name='todo_main'),
 )
--- todolist/views.py
+++ todolist/views.py
@@ -1,14 +1,49 @@
-from todolist.models import Event, EventClass
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from django.views.generic.base import TemplateView
+from todolist.models import Event, EventClass
+from todolist.forms import TodoUserForm
 
-class TodoIndexView(TemplateView):
-    title = None
+def todo_login(request):
+    """
+    If this is a POST request, try to login
+    with the given username and password
+    Otherwise, return the login page
+    """
+    error_info = ''
+    if request.POST:
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('todo_main'))
+        error_info = 'Your username and password doesn\'t match, please try again.'
+    return render_to_response('todo_login.html', \
+                              {'error_info': error_info, }, \
+                              RequestContext(request))
 
-    def get_context_data(self,**kwargs):
-        context = super(TodoIndexView, self).get_context_data(**kwargs)
-        title = self.title
-        context['title'] = title
-        return context
+def todo_reg(request):
+    """
+    If this is a POST request, try to register
+    the user with the given user information
+    Otherwise, return the register page
+    """
+    error_info = ''
+    if request.POST:
+        todo_user_form = TodoUserForm(request.POST)
+        error_info = todo_user_form.errors
+        if todo_user_form.is_valid():
+            new_user = todo_user_form.save()
+            return HttpResponseRedirect(reverse('todo_main'))
+    return render_to_response('todo_reg.html', \
+                              {'error_info': error_info, }, \
+                              RequestContext(request))
 
 def get_event_classes(request, user_id):
     """
