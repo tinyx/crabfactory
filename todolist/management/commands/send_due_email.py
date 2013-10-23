943bc84f7ae81b7fc50da857e9dd6099807b6518
Yitao <yitao@bitsighttech.com>
2013-10-23 16:49:09 -0400

    Finish the logical part of the alert cron job, remaining the email sending part

--- todolist/management/commands/send_due_email.py
+++ todolist/management/commands/send_due_email.py
@@ -1,10 +1,16 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+from todolist.models import Event
+from datetime import date, timedelta
+ALERT_DAYS = [timedelta(-1), timedelta(0), timedelta(1), timedelta(7), timedelta(30)]
 
 class Command(BaseCommand):
     help = """This is a cron job for sending event due alert email everyday.
             You should not run this manually."""
     def handle(self, *args, **options):
         for user in User.objects.all():
-            print user
+            for event in Event.objects.filter(eventclass__user=user):
+                if event.duedate - date.today() in ALERT_DAYS:
+                    print event
+                    # Here is where should send the email
         return
