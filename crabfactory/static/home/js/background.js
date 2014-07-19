0e9805fb9e9a707048ecf59d72181d3b7623397b
Yitao Xiong <hb1412@gmail.com>
2014-07-19 21:21:43 +0800

    Change fonts

--- crabfactory/static/home/js/background.js
+++ crabfactory/static/home/js/background.js
@@ -1,3 +1,19 @@
 $("window").load(function() {
     $("body").removeClass("preload");
 })
+
+var display_intro = function() {
+    alert("display_intro");
+}
+
+var display_skill = function() {
+    alert("display_skill");
+}
+
+var display_project = function() {
+    alert("display_project");
+}
+
+var display_social = function() {
+    alert("display_project");
+}
