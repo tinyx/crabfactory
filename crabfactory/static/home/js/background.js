8db3901a59e25057bbe7dd02560dfa3bd6134f19
Yitao Xiong <hb1412@gmail.com>
2014-07-29 22:11:09 +0800

    Circle charts animation added

--- crabfactory/static/home/js/background.js
+++ crabfactory/static/home/js/background.js
@@ -1,14 +1,9 @@
+var circle_chart_displayed = false;
+
 $(window).load(function() {
     $("body").removeClass("preload");
 
 });
-
-(function($) {
-    var s = skrollr.init({
-        render: function(data) {
-        }
-    });
-})(jQuery)
 
 $(function() {
     $(".knob").knob({
@@ -22,3 +17,36 @@ var scroll_to = function(anchor) {
     console.log(anchor);
     $.scrollTo(anchor, 500);
 };
+
+var circle_chart_content = $("#content_3");
+(function($) {
+    var s = skrollr.init({
+        render: function(data) {
+            if(!circle_chart_displayed) {
+                if(circle_chart_content.hasClass('skrollable-after')) {
+                    //display circle chart
+                    display_all_circle_charts();
+                    circle_chart_displayed = true;
+                }
+            }
+        }
+    });
+})(jQuery)
+
+var display_circle_chart = function(skill_id, skill_value) {
+    $(skill_id).animate({value: skill_value}, {
+        duration: 1000,
+        easing: 'swing',
+        step: function() {
+            $(skill_id).val(Math.ceil(this.value)).change();
+        }
+    });
+};
+
+var display_all_circle_charts = function() {
+    display_circle_chart('#html5', 75);
+    display_circle_chart('#css3', 65);
+    display_circle_chart('#javascript', 60);
+    display_circle_chart('#ui_design', 50);
+    display_circle_chart('#angularjs', 30);
+}
