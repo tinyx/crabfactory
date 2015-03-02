74a6cd0586dbb525340efb3555d64aad8b72fa56
TinyX <hb1412@gmail.com>
2015-03-02 21:40:14 +0000

    Added some meaningless email code

--- todolist/management/commands/send_due_email.py
+++ todolist/management/commands/send_due_email.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from todolist.models import Event
 from datetime import date, timedelta
-from django.core.mail import EmailMessage
+from django.core.mail import send_mail
 ALERT_DAYS = [timedelta(-1), timedelta(0), timedelta(1), timedelta(7), timedelta(30)]
 
 class Command(BaseCommand):
@@ -14,11 +14,10 @@ class Command(BaseCommand):
                 time_delta = event.duedate - date.today()
                 if time_delta in ALERT_DAYS:
                     if time_delta < timedelta(0):
-                        title = 'Event dued'
+                        subject = 'Event dued'
                         message = 'Your event %s dued at %s.' % (event.content, event.duedate)
                     else:
-                        title = 'Event will due soon'
+                        subject = 'Event will due soon'
                         message = 'Your event %s will due at %s' % (event.content, event.duedate)
-                    email = EmailMessage(title, message, to=[user.email])
-                    email.send()
+                    send_mail(subject, message, 'todo@crabfactory.com', [user.email])
         return
