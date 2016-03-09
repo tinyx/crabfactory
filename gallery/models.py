f84d974d75d79616428c1e9f9f7d26ecce8018eb
Yitao Xiong <yitao@bitsighttech.com>
2016-03-09 10:35:41 -0500

    Started new app

---
+++ gallery/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Image(models.model):
+    """
+    name =
+    description =
+    category =
+    order =
+    annotation =
+    file =
+    """
+    pass
+
+class Category(models.model):
+    """
+    name =
+    order =
+    """
+    pass
