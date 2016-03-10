f2e841d41d7d9448b6db0af102aa4ad1fc31d6c7
Yitao Xiong <yitao@bitsighttech.com>
2016-03-09 19:14:28 -0500

    Updated url

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,5 +3,5 @@ from django.contrib.auth.decorators import login_required
 from gallery.views import GalleryView
 
 urlpatterns = patterns('',
-    url(r'^(?P<category>[a-zA-Z0-9-]+)/$', GalleryView.as_view(), name='gallery_view'),
+    url(r'^(?P<category>[\w ]+)/$', GalleryView.as_view(), name='gallery_view'),
 )
