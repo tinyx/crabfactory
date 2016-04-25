bc49b9226fe74011938e72d21513bc72d3f93de0
Yitao Xiong <yitao@bitsighttech.com>
2016-04-25 16:22:46 -0400

    Added motion page for videos

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
-from gallery.views import GalleryView
+from gallery.views import GalleryView, MotionView
 
 urlpatterns = patterns('',
     url(r'^$', GalleryView.as_view(), name='gallery_view'),
-    url(r'^(?P<category>[\w ]+)/$', GalleryView.as_view(), name='gallery_view'),
+    url(r'^motion/$', MotionView.as_view(), name='motion_view'),
+    url(r'^(?P<category>[\w ]+)/$', GalleryView.as_view(), name='gallery_view')
 )
