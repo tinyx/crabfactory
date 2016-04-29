62351033dba141d7eea56609319f4a7555ca310c
Yitao Xiong <yitao@bitsighttech.com>
2016-04-29 14:43:44 -0400

    URL issue

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,7 +5,7 @@ from gallery.views import GalleryView, MotionView
 urlpatterns = patterns('',
     url(r'^cn/$', GalleryView.as_view(template_name='gallery_cn.html'), name='gallery_view'),
     url(r'^cn/motion/$', MotionView.as_view(template_name='motion_cn.html'), name='gallery_view'),
-    url(r'^cn/(?P<category>[\w ]+)/$', GalleryView.as_view(), name='gallery_view'),
+    url(r'^cn/(?P<category>[\w ]+)/$', GalleryView.as_view(template_name='gallery_cn.html'), name='gallery_view'),
     url(r'^$', GalleryView.as_view(), name='gallery_view'),
     url(r'^motion/$', MotionView.as_view(), name='motion_view'),
     url(r'^(?P<category>[\w ]+)/$', GalleryView.as_view(), name='gallery_view')
