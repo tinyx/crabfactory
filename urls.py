bfc9071697a93755b2447a859291ffb9934fdd99
Yitao Xiong <yitao@bitsighttech.com>
2016-12-16 16:56:07 -0500

    Save

--- urls.py
+++ urls.py
@@ -6,7 +6,6 @@ from django.views.generic import TemplateView
 from crabfactory.views import create_user
 from todolist.urls import urlpatterns as todolist_url
 from todo_service.urls import urlpatterns as todo_service_url
-from webresume.urls import urlpatterns as webresume_url
 from gallery.urls import urlpatterns as gallery_url
 
 # Uncomment the next two lines to enable the admin:
@@ -26,7 +25,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^todo/', include(todolist_url)),
     url(r'^todo-api/', include(todo_service_url)),
-    url(r'^webresume/', include(webresume_url)),
     url(r'gallery/', include(gallery_url)),
     url(r'^filer/', include('filer.urls')),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
