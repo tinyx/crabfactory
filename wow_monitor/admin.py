e317539e33b360eb1bd0b13f8bc2eb5ba4f85996
TinyX <hb1412@gmail.com>
2016-12-23 22:18:00 +0000

    Updated admin

--- wow_monitor/admin.py
+++ wow_monitor/admin.py
@@ -2,11 +2,8 @@ from django.contrib import admin
 
 from models import Character, SimcRank
 
-class CharacterAdmin(admin.ModelAdmin):
-    pass
-
 class SimcRankAdmin(admin.ModelAdmin):
-    readonly_fields = ('rating_time')
+    readonly_fields = ('rating_time',)
 
 admin.site.register(Character)
-admin.site.register(SimcRank)
+admin.site.register(SimcRank, SimcRankAdmin)
