af017ae9020a62fee1c816b7f3589fea5b7dadbe
TinyX <hb1412@gmail.com>
2016-12-23 19:05:43 +0000

    Save

--- wow_monitor/management/commands/refresh_characters.py
+++ wow_monitor/management/commands/refresh_characters.py
@@ -1,5 +1,10 @@
+import json
+
 from django.core.management.base import BaseCommand
+from django.conf import settings
+
 from wow_monitor.models import Character, SimcRank
+from subprocess import call
 
 class Command(BaseCommand):
     help = """This is a cron job for refreshing all wow characters."""
@@ -13,6 +18,17 @@ class Command(BaseCommand):
                 if new_c_dict[key] != old_c_dict[key]:
                     updated = True
                     # Run simc here
+                    call([settings.SIMC,
+                          'armory={server},{name}'.format(
+                              server=c.server_name, name=c.name
+                          ),
+                          'report_details=0',
+                          'json=simcTmp'])
+                    with open('simcTmp', 'r') as report:
+                        report_json = json.load(report)
+                        dps_rank = report_json['sim']['raid_dps']['mean']
+                        SimcRank.objects.create(character=c, dps_rank=dps_rank)
+                    break
             if not updated:
                 print 'Nothing changed for character {}'.format(c.name)
             else:
