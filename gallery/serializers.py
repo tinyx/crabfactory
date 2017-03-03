4e9e12d0dbb9490ee32b299a7615fbf315afd45d
TinyX <hb1412@gmail.com>
2017-03-03 19:52:31 +0000

    Revert "Save"
    This reverts commit 8eb55fa6cdd4cc20f948333dcd161fc3366c9e73.

--- gallery/serializers.py
+++ gallery/serializers.py
@@ -9,15 +9,7 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class ImageSerializer(serializers.ModelSerializer):
     image_file = serializers.SlugRelatedField(read_only=True, slug_field='url')
-    width = serializers.SerializerMethodField('get_image_width')
-    height = serializers.SerializerMethodField('get_image_height')
-
-    def get_image_width(self, obj):
-        return obj.image_file._width
-
-    def get_image_height(self, obj):
-        return obj.image_file._height
 
     class Meta:
         model = Image
-        fields = ('id', 'name', 'description', 'description_cn', 'category', 'order', 'annotation', 'image_file', 'width', 'height')
+        fields = ('id', 'name', 'description', 'description_cn', 'category', 'order', 'annotation', 'image_file')
